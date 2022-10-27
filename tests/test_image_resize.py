0b792c63e5943f7fb414819cacbd326fb5fc7a40
kaydxh <523520531@qq.com>
2022-10-27 19:53:20 +0800

    feat(image): resize and crop image

--- tests/test_image_resize.py
+++ tests/test_image_resize.py
@@ -7,12 +7,19 @@ import peek.cv.image.image as image_
 
 
 class TestImageResize(unittest.TestCase):
-    def test_resize_image_pad(self):
+    def test_resize_pad_image(self):
         img_path = "./testdata/test.jpg"
         inp_img = cv2.imread(img_path)
         # inp_img = inp_img.astype("float32")
-        target_img = image_.resize_image_pad(inp_img, 100, 200)
-        cv2.imwrite("./testdata/test_target_f.jpg", target_img)
+        target_img = image_.resize_pad_image(inp_img, 100, 200)
+        cv2.imwrite("./testdata/test_target_resize_pad.jpg", target_img)
+
+    def test_resize_crop_image(self):
+        img_path = "./testdata/test.jpg"
+        inp_img = cv2.imread(img_path)
+        # inp_img = inp_img.astype("float32")
+        target_img = image_.resize_crop_image(inp_img, 100, 200)
+        cv2.imwrite("./testdata/test_target_resize_crop.jpg", target_img)
 
 
 if __name__ == "__main__":
