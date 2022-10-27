258ea9d3c57874b76a0b15b05a83d0c5ec3f101f
kayxhding <kayxhding@tencent.com>
2022-10-27 17:03:30 +0800

    feat(torch): update inference

--- peek/cv/torch/transform.py
+++ peek/cv/torch/transform.py
@@ -6,13 +6,13 @@ import numpy as np
 import torchvision.transforms as transforms
 import torch
 
-def normalize_img(inp_img, target_width=256, target_height=256):
+def normalize_img_float32(inp_img, target_width=256, target_height=256):
     """ normalize_img normal distribution
     """
+    inp_img = inp_img.astype(np.float32)
     inp_img = image_.resize_image_pad(inp_img, target_width=target_width,
              target_height=target_height)
     # normalize
-    inp_img = inp_img.astype('float32')
     inp_img /= 255.0
     # turn a tensor of shape [width, height, channels] into [channels, height, width] for the CNN
     inp_img = np.transpose(inp_img, axes=(2, 0, 1))
@@ -22,9 +22,10 @@ def normalize_img(inp_img, target_width=256, target_height=256):
 		std=[0.229, 0.224, 0.225])(inp_img)
     return inp_img
 
-def normalize_img_ex(inp_img, target_width=256, target_height=256):
+def normalize_img_uint8(inp_img, target_width=256, target_height=256):
     """ normalize_img normal distribution
     """
+    inp_img = inp_img.astype(np.float32)
     inp_img = image_.resize_image_pad(inp_img, target_width=target_width,
              target_height=target_height)
     # normalize
@@ -37,6 +38,6 @@ def normalize_img_ex(inp_img, target_width=256, target_height=256):
 		     mean=[0.485, 0.456, 0.406],
 		     std=[0.229, 0.224, 0.225])
          ])
-    inp_img = noramlize_tranform(inp_img)
+    inp_img = noramlize_tranform(inp_img.astype(np.uint8))
     return inp_img
 
--- tests/test_torch_inference.py
+++ tests/test_torch_inference.py
@@ -4,6 +4,8 @@
 import unittest
 import os
 import cv2
+import numpy as np
+import torch
 from peek.cv.torch.device import get_avaliable_devices
 import peek.cv.torch.model as model_
 import peek.cv.torch.transform as transform_ 
@@ -17,12 +19,19 @@ class TestTorchInference(unittest.TestCase):
             return
         inp_img = cv2.imread(img_path)
 
-        model_path = "./testdata/saliency_traced.pt"
+        model_path = "./testdata/saliency_traced_cpu.pt"
         if not os.path.exists(model_path):
             print(f"{model_path} is not exist")
             return
-        model_.load_model_with_device_id(model_path, -1)
-        inp_img = transform_.normalize_img(inp_img)
+        loaded = model_.load_model_with_device_id(model_path, -1)
+        inp_img = transform_.normalize_img_float32(inp_img)
+        inp_img = inp_img.unsqueeze(0)
+
+        with torch.no_grad():
+        # pred_masks_raw = np.squeeze(pred_masks.cpu().numpy(), axis=(0, 1))
+            pred_masks, _ = loaded(inp_img)
+        print(pred_masks)
+        pred_masks_raw = np.squeeze(pred_masks.cpu().numpy(), axis=(0, 1))
 
 if __name__ == '__main__':
     unittest.main()
