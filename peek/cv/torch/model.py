c93e159288a0337b02a9a25efdbb320ca456dc9d
kaydxh <523520531@qq.com>
2022-10-27 14:53:19 +0800

    refactor(torch): update model load

--- peek/cv/torch/model.py
+++ peek/cv/torch/model.py
@@ -6,5 +6,8 @@ import peek.cv.torch.device as device_
 
 def load_model_with_device_id(path, device_id):
     device = device_.Device(device_id)
-    torch.load(path, map_location=device.get_device())
+    load = torch.load(path, map_location=device.get_device())
+    if device.is_cpu():
+       return load.cpu()
+    return load.cuda()
 
