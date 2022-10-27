fb31ac432720322fefe3e61a3e9039303cf7a82e
kaydxh <523520531@qq.com>
2022-10-27 19:22:06 +0800

    feat(image): add resize and crop image

--- peek/cv/image/image.py
+++ peek/cv/image/image.py
@@ -4,7 +4,7 @@
 import cv2
 
 
-def resize_image_pad(inp_img, target_width=None, target_height=None):
+def resize_pad_image(inp_img, target_width=None, target_height=None):
     """ resize_image_pad to resize and pad image to a given target size
     """
     h, w, c = inp_img.shape
@@ -28,3 +28,19 @@ def resize_image_pad(inp_img, target_width=None, target_height=None):
         border_img, (target_width, target_height), interpolation=cv2.INTER_AREA
     )
     return resize_img
+
+
+def resize_crop_image(inp_img, target_width=None, target_height=None):
+    """ resize_crop_image to resize and crop image
+    """
+    if (not target_width) or (not target_height):
+        return inp_img
+    h, w = target_width, target_height
+    size = max(h, w)
+    padding_h = (size - h) // 2
+    padding_w = (size - w) // 2
+
+    resize_img = cv2.resize(
+        inp_img, (w + 2 * padding_w, h + 2 * padding_h), interpolation=cv2.INTER_NEAREST
+    )
+    return resize_img[padding_h : padding_h + h, padding_w : padding_w + w]
