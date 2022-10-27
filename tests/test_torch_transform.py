5826fcfd361ff67a809573d33df7fbe21a7e391f
kaydxh <xiao_hua_ding@sina.cn>
2022-10-27 12:54:59 +0800

    refactor(torch): test transform

--- tests/test_torch_transform.py
+++ tests/test_torch_transform.py
@@ -10,14 +10,14 @@ import torchvision.transforms as transforms
 # export KMP_DUPLICATE_LIB_OK=True
 class TestTorchTransform(unittest.TestCase):
     def test_normalize_img(self):
-         img_path = "./testdata/test.jpg"
-         if not os.path.exists(img_path):
-             print(f"{img_path} is not exist")
-             return
-         inp_img = cv2.imread(img_path)
-         inp_img = transform_.normalize_img_ex(inp_img)
-         inp_img = transforms.ToPILImage()(inp_img).convert('RGB')
-         inp_img.show()
+        img_path = "./testdata/test.jpg"
+        if not os.path.exists(img_path):
+            print(f"{img_path} is not exist")
+            return
+        inp_img = cv2.imread(img_path)
+        inp_img = transform_.normalize_img_ex(inp_img)
+        inp_img = transforms.ToPILImage()(inp_img).convert('RGB')
+        inp_img.show()
 
 if __name__ == '__main__':
     unittest.main()
