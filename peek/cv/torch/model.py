621909e34bf19095471841f6e13e55c44c7a68bf
kaydxh <523520531@qq.com>
2022-10-27 17:03:30 +0800

    feat(torch): update inference

--- peek/cv/torch/model.py
+++ peek/cv/torch/model.py
@@ -6,8 +6,9 @@ import peek.cv.torch.device as device_
 
 def load_model_with_device_id(path, device_id):
     device = device_.Device(device_id)
-    load = torch.load(path, map_location=device.get_device())
+    model = torch.jit.load(path, map_location=device.get_device())
+    model.eval()
     if device.is_cpu():
-       return load.cpu()
-    return load.cuda()
+       return model.cpu()
+    return  model.cuda()
 
