e00f4099444f69166c97ea6c9eb54a16da5922ff
kayxhding <kayxhding@tencent.com>
2022-10-28 13:04:38 +0800

    refactor(torch): pre and post process

--- tests/test_torch_inference.py
+++ tests/test_torch_inference.py
@@ -14,6 +14,18 @@ import peek.cv.image.image as image_
 
 
 class TestTorchInference(unittest.TestCase):
+    def pre_process(self, inp_img):
+        inp_img = transform_.normalize_img_float32(inp_img)
+        inp_img = inp_img.unsqueeze(0)
+        return inp_img
+
+    def post_process(self, pred_masks, ori_w, ori_h):
+        pred_masks_raw = np.squeeze(pred_masks.numpy(), axis=(0, 1))
+        pred_masks_raw = image_.resize_crop_image(pred_masks_raw, ori_w, ori_h)
+        # print(f'pred_masks_raw: {pred_masks_raw}')
+        crop_box = image_.erode(pred_masks_raw, 5 / 4)
+        return crop_box
+
     def test_inference(self):
         model_path = "./testdata/saliency_traced_cpu.pt"
         if not os.path.exists(model_path):
@@ -29,20 +41,12 @@ class TestTorchInference(unittest.TestCase):
         inp_img = cv2.imread(img_path)
         h = inp_img.shape[0]
         w = inp_img.shape[1]
-        print(f"{w} {h}")
-        inp_img = transform_.normalize_img_float32(inp_img)
-        inp_img = inp_img.unsqueeze(0)
+        print(f"origin w[{w}] h[{h}]")
 
+        inp_img = self.pre_process(inp_img)
         pred_masks = inference_.forward(loaded, inp_img)
-        print(pred_masks)
-        pred_masks_raw = np.squeeze(pred_masks.numpy(), axis=(0, 1))
-        pred_masks_raw = image_.resize_crop_image(
-            pred_masks_raw, w, h
-        )
-        print(f'pred_masks_raw: {pred_masks_raw}')
-        crop_box = image_.erode(pred_masks_raw , 5/4)
-        print(f'crop_box: {crop_box}')
-
+        result_box = self.post_process(pred_masks, w, h)
+        print(f"result_box: {result_box}")
 
 
 if __name__ == "__main__":
