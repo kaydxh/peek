8f9fd78b698eaff50db8c850b01116cd6677d544
kaydxh <xiao_hua_ding@sina.cn>
2024-07-23 17:10:18 +0800

    feat(file): dump json

--- peek/os/file.py
+++ peek/os/file.py
@@ -2,13 +2,14 @@
 # -*- coding: utf-8 -*-
 
 import os
+import json
 
 def MakeDirAll(name):
     if not os.path.exists(name):
         os.makedirs(name, mode=0o755)
 
-
-def Dump(output_file, content):
+def DumpJson(output_file, json_data):
+    data = json.dumps(json_data, indent=4)
     with open(output_file, 'w') as f:
-        f.write(content)
+        f.write(data)
     
