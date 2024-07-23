0e814c6e9b613c598f5cbe3fca63a328af52ab50
kaydxh <523520531@qq.com>
2024-07-23 17:10:18 +0800

    feat(file): dump json

--- tests/test_os.py
+++ tests/test_os.py
@@ -5,13 +5,23 @@
 import unittest
 
 # sys.path.append("..")
-from peek.os.file import MakeDirAll
+# from peek.os.file import MakeDirAll
+import peek.os.file as file_
+import peek.encoding.base64.base64 as base64_
 
 
 class TestOsFile(unittest.TestCase):
     def test_make_dir_all(self):
-        MakeDirAll("mytest/0/1/2/3")
+         #file_.MakeDirAll("mytest/0/1/2/3")
+         pass
 
-
+    def test_dump(self):
+        data = {
+                "session_id": "session_id",
+                "image": base64_.encode("tests/testdata/test.jpg"),
+                }
+        
+        file_.DumpJson("tests/testdata/output_dump.json", data)
+    
 if __name__ == "__main__":
     unittest.main()
